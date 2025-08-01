bc297a30569335b932f3e0445b7eee5f169e0495
vvkrghv01 <vvkrghv01@gmail.com>
2025-08-01 18:53:14 +0530

    Thread methods updated

--- src/World.java
+++ src/World.java
@@ -16,6 +16,9 @@ public class World extends Thread{ //implements Runnable {
                 throw new RuntimeException(e);
                 //System.out.println("Exception in thread :" + e);
             }
+            Thread.yield();//hint to a scheduler current thread willing to yield its current use of processor and wants to give cpu time to other threads
+
+
            // System.out.println(i);
         }
 
@@ -25,6 +28,7 @@ public class World extends Thread{ //implements Runnable {
         World t1 = new World("vvkrghv");
         World t2 = new World("ash");
         World t3 = new World("priy");
+        t1.setDaemon(true);
         t1.start();// used to initialize the user thread;
         t1.setPriority(Thread.MIN_PRIORITY);
         t1.interrupt();
@@ -32,8 +36,8 @@ public class World extends Thread{ //implements Runnable {
         t2.setPriority(Thread.MAX_PRIORITY);
         t3.start();
         t3.setPriority(Thread.NORM_PRIORITY);
-        //t1.join();// by using join method the execution of main thread has stopped, and it waits until t1 thread does not terminate
+        t1.join();// by using join method the execution of main thread has stopped, and it waits until t1 thread does not terminate
                  // after that main thread will start its execution
-        //System.out.println("hello");
+        System.out.println("hello");
     }
 }
